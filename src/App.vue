d081b6572fab133ad9c60e7b57d05846b09de57f
licuicui <18317537300@163.com>
2024-08-04 01:49:23 +0800

    2222

--- src/App.vue
+++ src/App.vue
@@ -3,44 +3,11 @@ import PageOne from './views/PageOne.vue'
 </script>
 
 <template>
-  <!-- <A :value1="value1" @deca="dec" ref="child"/> -->
-  <PageOne></PageOne>
-
-  <hr/>
-  <!-- <div class="father">
-    <p>我是父亲</p>
-    <div class="son">我是儿子</div>
-  </div>
-  <div class="target">
-    <p>我是itarget
-      <span class="goal">我是目标3</span>
-    </p>
-    <div class="goal">我是目标</div>
-  </div>
-  <div class="goal">我是目标2</div> -->
- <!-- 哈哈哈哈 -->
+ 2222222222
 </template>
 
 <style scoped lang="less">
-// @import './assets/main.less';
-// .father{
-//   font-size:@fontSize;
-//   color:red;
-//   .border();
-//   .son{
-//     width:@width;
-//     background-color: purple;
-//     font-size:20px;
-//     color:@color
-//   }
-//   &:hover{
-//     background-color: green;
-//     color:right[color];
-//   }
-// }
-// .right{
-//   color:blue;
-// }
+
 
 
 </style>
